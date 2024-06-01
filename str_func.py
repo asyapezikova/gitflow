52f378faf375f4fe66308b18d592a1ba164abda6
Анастасия <bobina_asya@mail.ru>
2024-06-01 20:21:21 +0300

    new function title_line

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
-def upper(line):
+def upper_line(line):
+    """
+    Возвращает строку со всеми заглавными буквами
+    """
     return line.upper()
+
+def title_line(line):
+    """
+    Возвращает строку с заглавными первыми буквами каждого слова в строке
+    """
+    return line.title()
